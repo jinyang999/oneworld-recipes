b1ef78d6ff156712ddbd4f0fe7190901d383936b
yj <yj9420520@163.com>
2022-10-17 12:16:08 +0800

    修改

--- recipes/tchap/webview.js
+++
@@ -1,8 +0,0 @@
-module.exports = Ferdium => {
-  const getMessages = () => {
-    let count = document.querySelector('.mx_RoomSubList_badge').innerHTML
-    Ferdium.setBadge(count);
-  };
-
-  Ferdium.loop(getMessages);
-};
--- recipes/zammad/webview.js
+++
@@ -1,8 +0,0 @@
-module.exports = Ferdium => {
-  const getMessages = function getMessages() {
-    const notificationsCounter = document.querySelector('.js-notificationsCounter');
-    Ferdium.setBadge(Ferdium.safeParseInt(notificationsCounter.textContent));
-  };
-
-  Ferdium.loop(getMessages);
-};
